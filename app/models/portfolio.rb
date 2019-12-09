7f463f6955cc6ffc9a5f1a3470addb6b6af7c3d6
Cath Riddoch <cathriddoch@gmail.com>
2019-12-09 14:21:20 +0000

    adds nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,15 +1,16 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,reject_if:lambda{|attrs| attrs['name'].blank?}
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope:react, ->{where(subtitle:"React")}
   scope:ruby, ->{where(subtitle:"Ruby")}
 
   after_initialize :set_defaults
-
   def set_defaults
     self.main_image ||= "https://via.placeholder.com/600x400"
-    self.main_image ||= "https://via.placeholder.com/350x200"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
   end
 
 end
