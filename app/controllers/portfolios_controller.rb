7537f5b1fc7de2c7d1a8596afc20da116669754d
Cath Riddoch <cathriddoch@gmail.com>
2019-12-11 11:19:37 +0000

    fixes strange missing end where there isnt a missing end bug

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,69 +1,70 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
-    end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-    def react
-        @react_portfolio_items = Portfolio.react
-    end
+  def react
+    @react_portfolio_items = Portfolio.react
+  end
 
-    def ruby
-        @ruby_portfolio_items = Portfolio.ruby
-    end
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
 
-    def new
-      @portfolio_item = Portfolio.new
-      3.times{@portfolio_item.technologies.build}
-    end
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
 
-    def create
-      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
-      #@portfolio_item = Portfolio.new(portfolio_params)
-
-      respond_to do |format|
-        if @portfolio_item.save
-          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        else
-          format.html { render :new }
-
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
       end
     end
+  end
 
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-    def update
-      @portfolio_item = Portfolio.find(params[:id])
-      respond_to do |format|
-        if @portfolio_item.update(portfolio_params)
-          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
 
-        else
-          format.html { render :edit }
-
-        end
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
+  end
 
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
+  end
 
-    def destroy
-      @portfolio_item = Portfolio.find(params[:id])
-      @portfolio_item.destroy
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
-    end
+  private
 
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
 
-
-
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def portfolio_params
-        params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes:[:name])
-      end
-    end
 end
