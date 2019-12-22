d8c764a61cdba5ce31004afdfc6146e9285fe9da
Cath Riddoch <cathriddoch@gmail.com>
2019-12-22 10:06:55 +0000

    fixes buttons on portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ puts "5 skills created"
         subtitle:"Ruby",
         body:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         main_image: "https://via.placeholder.com/600x400",
-        thumb_image: "https://via.placeholder.com/350x200",
+        thumb_image: "https://via.placeholder.com/356x280",
     )
 end
 
@@ -48,7 +48,7 @@ end
         subtitle:"React",
         body:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         main_image: "https://via.placeholder.com/600x400",
-        thumb_image: "https://via.placeholder.com/350x200",
+        thumb_image: "https://via.placeholder.com/356x280",
     )
 end
 puts "9 portfolio items created"
