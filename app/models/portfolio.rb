92cbea19e39a5b9fea1627ae7d8a1cb46bc3f2fc
Cath Riddoch <cathriddoch@gmail.com>
2020-01-03 19:14:02 +0000

    sets up AWS storage for images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -12,8 +12,8 @@ class Portfolio < ApplicationRecord
 
   after_initialize :set_defaults
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/356x280"
+    # self.main_image ||= "https://via.placeholder.com/600x400"
+    # self.thumb_image ||= "https://via.placeholder.com/356x280"
   end
 
 
