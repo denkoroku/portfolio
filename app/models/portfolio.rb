d78f073926a1e794f52bfd564c288bab01c97dfa
Cath Riddoch <cathriddoch@gmail.com>
2020-01-03 20:14:50 +0000

    sorts default images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,20 +2,12 @@ class Portfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies,reject_if:lambda{|attrs| attrs['name'].blank?}
 
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
 
   scope:react, ->{where(subtitle:"React")}
   scope:ruby, ->{where(subtitle:"Ruby")}
-
-  after_initialize :set_defaults
-  def set_defaults
-    # self.main_image ||= "https://via.placeholder.com/600x400"
-    # self.thumb_image ||= "https://via.placeholder.com/356x280"
-  end
-
-
 
 end
